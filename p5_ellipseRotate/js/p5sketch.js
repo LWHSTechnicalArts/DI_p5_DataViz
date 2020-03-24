c461349a971256eaaaa5dfd1173452935309e25b
Andrew Kleindolph <akleindolph@gmail.com>
2020-03-24 15:11:29 -0700

    Update p5sketch.js

--- p5_ellipseRotate/js/p5sketch.js
+++ p5_ellipseRotate/js/p5sketch.js
@@ -1,16 +1,16 @@
 // Given the CSV file "data.csv"
 // in the project's "assets" folder:
 
-var table;
-var row;
-var time;
-var sensor;
+let table;
+let row;
+let time;
+let sensor;
 
-var radius = 250;
-var angle = 0;
+let radius = 250;   //this is the radius of the ring of ellipses
+let angle = 0;
 
-var x;
-var y;
+let x;
+let y;
 
 function preload() {
     //table is comma separated value "csv"
@@ -22,10 +22,10 @@ function setup() {
     //setup look
     createCanvas(1000, 600);
     background(30, 140, 250);
-    translate(width/2, height/2);
+    translate(width/2, height/2);    //this will make your zero,zero point be the center of the canvas, not the upper left corner
     ellipseMode(CENTER);
+    
     //location variables
-
     x = 0;
     y = 0;
     angle = TWO_PI/(table.getRowCount());   //set rotation angle, TW0_PI is a full circle
@@ -41,21 +41,20 @@ function setup() {
         print(time); //optional but helpful
         print(sensor);
 
-        time = map(time, 0, 50, 30, 255); //remap the time variable
-        sensor = map(sensor, 40, 30000, 10, (width / table.getRowCount()) * 2); //remap the sensor variable
+        mapped_time = map(time, 0, 50, 30, 255); //remap the time variable
+        mapped_sensor = map(sensor, 40, 30000, 10, (width / table.getRowCount()) * 2); //remap the sensor variable
         //look of ellipses
-        fill(0, time, time, 220); //time changes the fill color
+        fill(0, mapped_time, mapped_time, 220); //time changes the fill color
         strokeWeight(1);
-        stroke(time);
+        stroke(mapped_time);    //change the stroke color as the time increases.
 
-        line(0,0,x - radius, y)
-        ellipse(x - radius, y, sensor, sensor);
+        //line(0,0,x - radius, y)
+        ellipse(x - radius, y, mapped_sensor, mapped_sensor);
 
 
         textAlign(CENTER);
         fill(255);
-        text(parseInt(sensor), x - radius/1.4, y); //printing sensor value as int to avoid decimal places
-
+        text(parseInt(sensor), x - radius/1.2, y); //printing sensor value as int to avoid decimal places
         rotate(angle);    //rotate before drawing next ellipse
     }
 }
