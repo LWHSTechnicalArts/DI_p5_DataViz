d172b0959c9ce6ba78c894a7c64bb5afc14a3f23
Andrew Kleindolph <akleindolph@gmail.com>
2020-03-24 11:01:08 -0700

    Update p5sketch.js

--- p5_dataSpheres/js/p5sketch.js
+++ p5_dataSpheres/js/p5sketch.js
@@ -1,15 +1,13 @@
 // Given the CSV file "data.csv"
 // in the project's "assets" folder:
 
-var table;
-var row;
-var r = 0;
-var time;
-var sensor;
+let table;
+let row;
+let time;
+let sensor;
 
-var x = 0;
-var y = 0;
-var z = 0;
+let x;
+let y;
 
 function preload() {
     //table is comma separated value "csv"
@@ -18,50 +16,39 @@ function preload() {
 }
 
 function setup() {
-    //setup look
-    createCanvas(1000, 800, WEBGL);
-    noStroke();
-    fill(204, 120);
-    background(0);
-    frameRate(30);
+  //setup look of the background
+  createCanvas(1000, 600); //canvas size in pixels
+  background(100, 70, 200); //background color in RGB
+  ellipseMode(CENTER); //draw ellipses from the center
+
+  x = 10;  // set a starting x value that is 10 pixels from the left
+  y = height / 2; //set a starting y value in the center of the canvas
+
+  for (let r = 0; r < table.getRowCount(); r++) {    //iterate thorough all rows of CSV file using a for loop, note: a row is an object, not an array
+    row = table.getRow(r);
+    time = row.getNum(0);     //grab the time value from column 0
+    sensor = row.getNum(1);   //grab the sensor value from column 1
+
+    print(time); //optional but helpful
+    print(sensor);
+
+    let mapped_time = map(time, 0, 50, 0, 255); //remap the time variable so the ellipses are bigger. first two numbers are original range, 2nd two are mapped range
+    let mapped_sensor = map(sensor, 40, 30000, 10, (width / table.getRowCount()) * 2); //remap the sensor variable
+    
+    //look of ellipses
+    fill(200, 100, 150); //time changes the fill color
+    strokeWeight(3);    // stroke size for ellipses
+    stroke(0);   //outline color for the ellipses
+
+    ellipse(x + mapped_sensor / 2, y, mapped_sensor, mapped_sensor); //draw ellipse exactly where previous ellipse ends with a width and height the size of the data
+
+    textAlign(CENTER);   //draw text from center
+    fill(255);  //this will be the color of the text can be RGBA or Grayscale
+    text(parseInt(sensor), x + mapped_sensor / 2, y); //printing sensor value as int to avoid decimal places
+    x += mapped_sensor; // add to the current value of x so the next ellipse is further to the right
+  }
 }
 
 function draw() {
-
-    orbitControl();    //allows mouse drags
-
-    background(250);
-    rotateY(frameCount * 0.01);
-
-    for (var r = 0; r < table.getRowCount(); r++) {
-        row = table.getRow(r);
-        
-        time = row.getNum(0);
-        sensor = row.getNum(1);
-
-        print(time); //optional but helpful
-        print(sensor);
-
-        time = map(time, 0, 50, 0, 255); //remap the time variable
-        sensor = map(sensor, 40, 30000, 10, (width / table.getRowCount()) * 2); //remap the sensor variable
-
-        x = time;
-        y = 0;
-        z = 0;
-
-        var sd = 60; // Define a standard deviation
-
-        x = (x * 3) 
-        y = (y * 2)  
-
-
-        push();
-        translate(x, y, z);
-        rotateX(-0.5);
-        rotateY(1);
-        normalMaterial();
-        sphere(sensor);
-        pop();
-    }
-
+    //nothing here
 }
